97bc63595ff989c2b926baa0735a3f71dfd0fd75
ShivamKumar1107 <ps.shivamkumar@gmail.com>
2024-03-29 01:05:22 +0530

    Implemented File upload middleware and service, i.e: multer and cloudinary

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,15 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) { // cb is callback function
+        cb(null, "/public/temp") // null is error, if there is no error, then null is passed
+    },
+    filename: function (req, file, cb) { // cb is callback function
+        // const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9) // generating unique file name
+        // cb(null, file.fieldname + '-' + uniqueSuffix) // null is error, if there is no error, then null is passed
+        cb(null, file.originalname) // null is error, if there is no error, then null is passed
+    }
+}); // diskStorage is used to store files on disk, destination is used to specify the folder where files will be stored, filename is used to specify the name of the file
+
+// const upload = multer({ storage: storage }) or in es6
+const upload = multer({ storage }) // multer is used to upload files
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,47 @@
+import {v2 as cloudinary} from 'cloudinary';
+import fs from 'fs';
+          
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET 
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try{
+        if(!localPath) return null;
+        // upload file on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {resource_type: "auto"});
+        // file uploaded successfully
+        console.log("File uploaded successfully");
+        console.log(result);
+        return response;
+    }
+    catch(error){
+        console.log("Error uploading file on cloudinary");
+        console.log(error);
+        fs.unlinkSync(localFilePath);
+        // delete the file from local storage. preferred to use unlinkSync instead of unlink as it is synchronous
+        return null;
+    }
+};
+
+export {uploadOnCloudinary};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// cloudinary.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
+//   { public_id: "olympic_flag" }, 
+//   function(error, result) {console.log(result); });
